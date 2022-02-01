7fe91c54ad90bc115c991c88a68ea373c0affa57
Tristan <LeoDog896@hotmail.com>
2022-02-01 13:35:38 -0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Tailwindcss from "./Tailwindcss.svelte"
+
+	let url: string
+
+</script>
+
+<Tailwindcss />
+<input bind:value={url} placeholder="Type in a URL">
+<button on:click={() => location.href = url}></button>
